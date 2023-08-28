555986b77df15441051da4a74a6b2806a1052aaa
rspforhp <petkermihail@gmail.com>
2023-08-29 00:06:45 +0500

    Initial commit

---
+++ Extension.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TestPopcapBetaStuff;
+
+public static class Extension
+{
+    public static string MD5Hash(this string s)
+    {
+        byte[] ansi = Encoding.ASCII.GetBytes(s);
+        byte[] hash = MD5.HashData(ansi);
+        var r= Convert.ToHexString(hash);
+        return r;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System.Security.Cryptography;
+using TestPopcapBetaStuff;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+Dictionary<string, string> nameToPassDictionary =new Dictionary<string, string>
+{
+    { "test", "test2".MD5Hash() }
+};
+app.MapGet("/beta_validate.php", (HttpRequest request) =>
+{
+    QueryString queryString = request.QueryString;
+    var t = queryString.ToString().Split("&");
+    t[0]=t[0].Remove(0,1);
+    string prod=null;
+    string ver=null;
+    string name=null;
+    string password=null;
+    foreach (var pair in t)
+    {
+        var p = pair.Split('=');
+        var _name = p[0];
+        var _value = p[1];
+        switch (_name)
+        {
+            case "prod": prod = _value;
+                break;
+            case "version": ver = _value;
+                break;
+            case "username": name = _value;
+                break;
+            case "pass": password = _value;
+                break;
+        }
+    }
+
+    if (name != null)
+    {
+        var realPass = nameToPassDictionary[name];
+        if (realPass.Equals(password, StringComparison.OrdinalIgnoreCase))
+        {
+            return "SUCCEEDED";
+        }
+    }
+    return "FAILED";
+});
+app.Run();
